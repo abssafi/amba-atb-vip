7b8da3a3e19c327c3b9e66aff3234b952fb4a362
Muhammad Qasim <developermqasim@outlook.com>
2025-07-18 16:47:59 +0500

    ran test sequence

--- tx_uvc/tb/tx_test.sv
+++ tx_uvc/tb/tx_test.sv
@@ -8,8 +8,8 @@ class tx_test extends uvm_test;
     endfunction: new
 
     function void build_phase (uvm_phase phase);
+        uvm_config_wrapper::set(this, "top_env.tx_uvc.agent.sequencer.run_phase", "default_sequence", tx_sequence::get_type());
         top_env = tx_top_env::type_id::create("top_env", this);
-        uvm_config_wrapper::set(this, "tx_uvc.agent.sequencer.run_phase", "default_sequence", simple_test::get_type());
         `uvm_info(get_type_name, "BUILD PHASE RUNNING...", UVM_LOW)
         super.build_phase(phase);
     endfunction: build_phase
