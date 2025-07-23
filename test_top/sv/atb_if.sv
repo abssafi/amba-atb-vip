74de04b6c99605788b90e06152b22bc1cc5770ec
Abdur Rafay <a.rafay9999@gmail.com>
2025-07-23 23:12:20 +0500

    assertions modified

--- test_top/sv/atb_if.sv
+++ test_top/sv/atb_if.sv
@@ -19,15 +19,15 @@ interface atb_if (input bit atclk, input bit atresetn);
 // a1: assert property (first) else $fatal("first asserion failed"); 
 // a2: assert property (second) else $fatal("second asserion failed"); 
 // a3: assert property (third) else $fatal("third asserion failed"); 
-a5: assert property (fifth) else $fatal("fifth assertion failed");
+// a5: assert property (fifth) else $fatal("fifth assertion failed");
+// a6: assert property (sixth) else $fatal("sixth assertion failed");
+a7: assert property (seventh) else $fatal("seventh assertion failed");
 
 
 
-
-
-property first; 
-(@(posedge atclk) (atvalid |-> (atbytes==4)));
-endproperty
+// property first; 
+// (@(posedge atclk) (atvalid |-> (atbytes==4)));
+// endproperty
 
 
 // property second;
@@ -44,9 +44,20 @@ endproperty
 
 //data should be zero when atvalid is 0. 
 
-property fifth;
-(@(posedge atclk) (atvalid == 0 |-> atdata == 0));
+// property fifth;
+// (@(posedge atclk) (atvalid == 0 |-> atdata == 0));
+//endproperty
 
+// property sixth;
+//  @(posedge atclk) (afvalid && afready |=> !afvalid) ;
+// endproperty
+
+
+property seventh;
+@(posedge atclk) (atvalid && atready) |->
+      !((atid == 7'h00) || ((atid >= 7'h70) && (atid <= 7'h7C)) || (atid == 7'h7E) || (atid == 7'h7F));
 endproperty
+
+
 
 endinterface : atb_if
