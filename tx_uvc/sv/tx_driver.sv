e06ecb2be153942f0dd258e0b96adf1eb3999ab5
Muhammad Qasim <developermqasim@outlook.com>
2025-07-23 14:02:48 +0500

    tx_driver tasks

--- tx_uvc/sv/tx_driver.sv
+++ tx_uvc/sv/tx_driver.sv
@@ -37,73 +37,13 @@ class tx_driver extends uvm_driver #(tx_packet);
         forever begin
             
             @(posedge vif.atclk);
-            seq_item_port.get_next_item(req);
-            trace_q.push_back(req.trace_data);
-            req.atvalid = 0;
-            vif.atvalid = 0;
-            
-
-            if(trace_q.size() == 4) begin
-                req.atdata[7:0] = trace_q.pop_front();
-                req.atdata[15:8] = trace_q.pop_front();
-                req.atdata[23:16] = trace_q.pop_front();
-                req.atdata[31:24] = trace_q.pop_front();
-                req.atvalid = 1;
-                vif.atvalid = 1;
-                req.atbytes = 4;
-                vif.atbytes = 4;
-                
-            end
-
-            //flush logic, but incomplete, need to assert afreadt == 1 as well.
-            if (vif.afvalid == 1 && trace_q.size() > 0) begin
-                //req.atdata = 0;
-                req.atbytes = 0;
-
-                // for (int i = 0, i < 4, i++) begin 
-                //     while (trace_q.size() != 0) begin
-                //         if (i == 0)
-                //             req.atdata[7:0] = trace_q.pop_front();
-                //         if (i == 1)
-                //             req.atdata[15:8] = trace_q.pop_front();
-                //         if (i == 2)
-                //             req.atdata[23:16] = trace_q.pop_front();
-                //         if (i == 3)
-                //             req.atdata[31:24] = trace_q.pop_front(); 
-                //         byte_n++;
-                //     end
-                // end
-
-                for (int i = 0; i < 4 && trace_q.size() > 0; i++) begin
-                    bte = trace_q.pop_front();
-                    $display("Popped and stored in bte : %h", bte);
-                    case (i)
-                        0: req.atdata[7:0] = bte;
-                        1: req.atdata[15:8] = bte;
-                        2: req.atdata[23:16] = bte;
-                        3: req.atdata[31:24] = bte;
-                    endcase
-                    byte_n++;
-                end
-                vif.atbytes = byte_n;
-                req.atbytes = byte_n;
-                vif.atvalid = 1;
-                req.atvalid = 1;
-                //wait (vif.atready);
-                send_to_dut(req);
-                req.atvalid = 0;
-                req.afready = 1;
-                flush_packets++;
-            end
+            get_packet();
+            full_data_packet();
+            flush_signal_assert();
 
             tx_coverage_collect.write(req);
 
-            if(vif.atvalid) begin
-                req.atbytes = 4;
-                tx_q.push_back(req);
-                atvalid_n++;
-                //`uvm_info(get_type_name(), $sformatf("TX Packet Stored (atvalid high): \n%s", req.sprint()), UVM_LOW)   
-            end
+            queue_packet_if_valid();
 
             if (vif.atready && vif.atvalid) begin
                 $display("sending to bus (from tx)");
@@ -133,9 +73,92 @@ class tx_driver extends uvm_driver #(tx_packet);
         `uvm_fatal("NOVIF", "VIF in DRIVER is NOT SET")
     endfunction: connect_phase
 
-//------------------------------------------------------
-//                  Driver Methods
-//------------------------------------------------------
+//==========================================================================
+//                  get_packets()
+//==========================================================================
+
+    task get_packet();
+        seq_item_port.get_next_item(req);
+        trace_q.push_back(req.trace_data);
+        req.atvalid = 0;
+        vif.atvalid = 0;
+    endtask: get_packet
+
+//==========================================================================
+//                  full_data_packet()
+//==========================================================================
+
+    task full_data_packet();
+        if(trace_q.size() == 4) begin
+            req.atdata[7:0] = trace_q.pop_front();
+            req.atdata[15:8] = trace_q.pop_front();
+            req.atdata[23:16] = trace_q.pop_front();
+            req.atdata[31:24] = trace_q.pop_front();
+            req.atvalid = 1;
+            vif.atvalid = 1;
+            req.atbytes = 4;
+            vif.atbytes = 4;
+        end
+    endtask: full_data_packet
+
+//==========================================================================
+//                  flush_signal_assert
+//==========================================================================
+
+    task flush_signal_assert();
+    if (vif.afvalid == 1 && trace_q.size() > 0) begin
+        req.atbytes = 0;
+        for (int i = 0; i < 4 && trace_q.size() > 0; i++) begin
+            bte = trace_q.pop_front();
+            $display("Popped and stored in bte : %h", bte);
+            case (i)
+                0: req.atdata[7:0] = bte;
+                1: req.atdata[15:8] = bte;
+                2: req.atdata[23:16] = bte;
+                3: req.atdata[31:24] = bte;
+            endcase
+            byte_n++;
+        end
+        vif.atbytes = byte_n;
+        req.atbytes = byte_n;
+        vif.atvalid = 1;
+        req.atvalid = 1;
+        //wait (vif.atready);
+        send_to_dut(req);
+        req.atvalid = 0;
+        req.afready = 1;
+        flush_packets++;
+    end
+    endtask: flush_signal_assert
+
+//==========================================================================
+//                 store_queue_packet()
+//==========================================================================
+
+    task queue_packet_if_valid();
+        if(vif.atvalid) begin
+            req.atbytes = 4;
+            tx_q.push_back(req);
+            atvalid_n++;
+        end
+    endtask: queue_packet_if_valid
+
+//==========================================================================
+//                 send_packet_if_ready()
+//==========================================================================
+
+    task send_packet_if_ready();
+        if (vif.atready && vif.atvalid) begin
+            $display("sending to bus (from tx)");
+            send_p = tx_q.pop_front();
+            send_to_dut(send_p);
+            sent_packets++;
+        end
+    endtask: send_packet_if_ready
+
+//==========================================================================
+//                 send_to_dut
+//==========================================================================
 
     task send_to_dut(tx_packet req);
         `uvm_info(get_type_name(), "send_to_dut() CALLED", UVM_LOW)
@@ -148,6 +171,10 @@ class tx_driver extends uvm_driver #(tx_packet);
         //vif.atwakeup = req.atwakeup;
         //`uvm_info(get_type_name(), $sformatf("Transaction # %0d - Packet SENT: \n%s", count+1, req.sprint()), UVM_LOW)   
     endtask: send_to_dut
+
+//==========================================================================
+//                 reset_signals()
+//==========================================================================
 
     task reset_signals();
         vif.atdata = 0;
